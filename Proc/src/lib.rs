57531d4440a72877d1710f61f4c54d929581c02f
Maxim Savenkov <max_savenkov@mail.ru>
2023-09-26 14:57:15 +0200

    custom IO

--- Proc/src/lib.rs
+++ Proc/src/lib.rs
@@ -16,10 +16,10 @@ pub fn main(_attrs: TokenStream, input: TokenStream) -> TokenStream {
         pub extern "C" fn mvrocket_launch(api: *const mvrocketlib::API, id: *const mvrocketlib::Uuid) {
             mvrocketlib::init(api, id);
             let rpcout = mvrocketlib::RpcOut::new();
-            let orig_stdout = io::stdout();
+            let orig_stdout = std::io::stdout();
             let orig_stdout_handle = orig_stdout.lock();
 
-            io::set_print(Some(Box::new(rpcout)));
+            std::io::set_print(Some(Box::new(rpcout)));
             #fn_name();
         }
     };
