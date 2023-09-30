2c02db1be5e7510fedb3fb96b211d5452479d0bf
Maxim Savenkov <max_savenkov@mail.ru>
2023-10-01 00:53:48 +0200

    Update

--- Proc/src/lib.rs
+++ Proc/src/lib.rs
@@ -3,20 +3,15 @@ use quote::quote;
 use syn::{parse_macro_input, ItemFn};
 #[proc_macro_attribute]
 pub fn main(_attrs: TokenStream, input: TokenStream) -> TokenStream {
-    // Parse the input function
     let input_fn = parse_macro_input!(input as ItemFn);
 
-    // Extract the function name
     let fn_name = &input_fn.sig.ident;
 
     let output = quote! {
-        use mvrocketlib::{print, println};
-
         #input_fn
 
         #[no_mangle]
-        pub extern "C" fn mvrocket_launch(api: *const mvrocketlib::API, id: *const mvrocketlib::Uuid) {
-            mvrocketlib::init(api, id);
+        pub extern "C" fn mvrocket_launch() {
             #fn_name();
         }
     };
